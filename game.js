cc9d261aa7feaaff4da2ad4496898cde7635499e
Riley <apolloisdumb@gmail.com>
2018-10-13 21:58:00 -0500

    Summary

---
+++ game.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let acorns = 0;
+let dontclick = 0;
+let boughtBlue = false;
+// why doesn't this work without settimeout -_-
+
+setTimeout(function(){ document.getElementById("acorns").innerHTML = acorns; }, 0);
+setInterval(function(){
+		acorns++;
+		document.getElementById("acorns").innerHTML = acorns;
+		if (acorns >= 100){
+			document.getElementById("beetleArrival").style.display = "";
+		}
+		if (acorns >= 250){
+			document.getElementById("lennon").style.display = "";
+		}
+	}, 1000);
+	
+function DontClick(){
+	acorns = acorns + 10;
+	document.getElementById("acorns").innerHTML = acorns;
+	dontclick++;
+	if (dontclick == 10) {
+		alert("Did you even read the button?!?!");
+	}
+}
+
+function buyBlue(){
+	if(boughtBlue == false && acorns >= 500){
+		acorns = acorns - 500;
+		document.body.style.backgroundColor = "#84b3ff";
+		boughtBlue = true;
+	}else if(acorns <=500){
+		alert("You cannot afford this, fool");
+	}else{
+		alert("You have already bought this upgrade, fool");
+	}
+}
